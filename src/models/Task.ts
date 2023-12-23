a9a6016cbc476d10f86f33b1ac4eda37bd959d5a
Artur Dantas <arturdantasrodrigues@gmail.com>
2023-12-23 10:01:47 -0300

    chore: adding a constraint to the priority of a task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,7 +2,7 @@ export interface Task {
   id: number,
   user_id: number,
   task_name: string,
-  priority: number,
+  priority: 1 | 2 | 3, // high | medium | low
   completed: boolean,
   created_at: Date,
   due_date?: Date
