5346a06fbf7e531b18195c220492f1b1e02017d8
Artur Dantas <arturdantasrodrigues@gmail.com>
2023-12-23 09:17:56 -0300

    chore: adding environment variables

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
+import 'dotenv/config';
 
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'todo',
-  password: 'root',
-  port: 5432,
+  user: process.env.POSTGRES_DB_USER,
+  host: process.env.POSTGRES_DB_HOST,
+  database: process.env.POSTGRES_DB_NAME,
+  password: process.env.POSTGRES_DB_PASSWORD,
+  port: Number(process.env.POSTGRES_DB_PORT) || 5432,
 })
 
 pool.on('error', (err, client) => {
