b32e29314e6d34ce744ca5db7e4081f09c813f2a
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-01-08 14:36:04 -0300

    feat: modelo de usuário, implementado

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const getUserSchema = z.object({
+  id: z.string().uuid(),
+  username: z.string(),
+  email: z.string().email(),
+  password_hash: z.string().min(8),
+});
+
+export type getUserDTO = z.infer<typeof getUserSchema>;
+
+export const createUserSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export type createUserDTO = z.infer<typeof createUserSchema>;
+
+export const updateUserSchema = z.object({
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(8).optional(),
+});
+
+export type updateUserDTO = z.infer<typeof updateUserSchema>;
