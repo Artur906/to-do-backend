a851540bd18805c3f6622e9b37e7f8ec1d07b1bd
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-01-20 18:00:03 -0300

    feat: authentication middleware

---
+++ src/middlewares/authHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import { object } from "zod";
+
+export const authHandler = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ auth: false, message: "Nenhum token foi informado" })
+
+  const jwtSecretKey = process.env.JWT_SECRET_KEY!;
+
+  jwt.verify(token, jwtSecretKey, (err, decoded) => {
+    if (err) return res.status(500).json({ auth: false, message: "Falha ao autenticar usuário" })
+
+    console.log("token decoded: ", decoded)
+    next()
+  })
+}
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import * as taskController from "../controllers/taskController";
 import { validate } from "../utils/validateSchema";
 import { createTaskSchema, updateTaskSchema } from "../models/Task";
+import { authHandler } from "../middlewares/authHandler";
 
 export const router = Router();
 
-router.get('/tasks', taskController.getAllTasks);
-router.get('/tasks/:taskId', taskController.getTaskById);
-router.post('/tasks', validate(createTaskSchema), taskController.createTask);
-router.put('/tasks/:taskId', validate(updateTaskSchema), taskController.updateTask);
+router.get('/tasks', authHandler, taskController.getAllTasks);
+router.get('/tasks/:taskId', authHandler, taskController.getTaskById);
+router.post('/tasks', authHandler, validate(createTaskSchema), taskController.createTask);
+router.put('/tasks/:taskId', authHandler, validate(updateTaskSchema), taskController.updateTask);
