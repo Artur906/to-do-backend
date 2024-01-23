055a653b7dd047f39e61e3a76c149edb672db171
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-01-23 10:47:49 -0300

    fix: using environment variable to set the port where server run

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import 'dotenv/config';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
