16978a49fb6887ad559130810705c9f83c8fbb7b
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-02-16 13:59:09 -0300

    chore: add cors
    cors let us connect other aplications with our API

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { router as taskRoutes } from './routes/taskRoutes';
 import { router as userRoutes } from './routes/userRoutes';
 import { errorHandler } from './middlewares/errorHandler';
+import cors from 'cors';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(taskRoutes);
 app.use(userRoutes);
